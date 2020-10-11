68ecb522abbd9bc5686af1dc67f9869f5332b3f7
Khasim Vali <khasimvalishaik72@gmail.com>
2020-10-11 12:11:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import {MatCardModule} from '@angular/material/card';
+import { QualificationComponent } from './qualification/qualification.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ShareIconsModule } from 'ngx-sharebuttons/icons';
+import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import {MatIconModule} from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    QualificationComponent,
+    SkillsComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTabsModule,
+    ShareIconsModule,
+    ShareButtonsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
